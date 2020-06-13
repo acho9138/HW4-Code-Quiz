29a80c7a0f19d749d6cf6f734ff15fe3cdbe8a0b
Anna C <anna.s.chong@gmail.com>
2020-06-13 23:54:57 +1000

    ADD Comments

--- finalScore.js
+++ finalScore.js
@@ -2,17 +2,19 @@ const timeLeft = document.querySelector('#timeLeft');
 const score = document.querySelector('#score');
 const initials = document.querySelector('#initials');
 const submit = document.querySelector('#submit');
-let finalScore = sessionStorage.getItem('score');
 
+// Display time remaining and score from session storage
+let finalScore = sessionStorage.getItem('score');
 timeLeft.textContent = finalScore;
 score.textContent = finalScore;
 
+// Store user score with initials in local storage and go to highscore page
 submit.addEventListener('click', storeHighscore);
 
-// Store highscore with initials in local storage and go to highscore page
 function storeHighscore(event) {
     event.preventDefault();
     let highscores = []
+    // Concat array with user score from local storage with new score
     highscores = highscores.concat(JSON.parse(localStorage.getItem('highscores')))
     let storeInitials = initials.value.toUpperCase();
     highscores.push(`${storeInitials}: ${finalScore}`);
